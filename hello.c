07c3bb1227de1b3d342cf62e543c30f806077180
Your name <you@example.com>
2023-05-24 09:44:00 -0700

    first

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is the entry point of the program
+ * return 0-indicate successful execition
+ */
+
+
+int main(void)
+{
+	printf("Hello world!, \n");
+	return (0);
+}
